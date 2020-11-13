3144a33ce26c65c99badf8e147e867f2945735f9
tvylormvde <wernerowy@gmail.com>
2020-11-13 18:22:54 +0100

    Add AmazonAPI and GenerateReport classes.

--- scraper.py
+++ scraper.py
@@ -15,3 +15,29 @@ from config import (
     FILTERS,
     URL
 )
+
+class GenerateReport:
+    def __init__(self):
+        pass
+
+
+
+
+class AmazonAPI:
+    def __init__(self, search, filters, url, currency):
+        self.search = search
+        self.url = url
+        self.price_filter = f"&rh=p_36%3A{filters["min"]}00-{filters["max"]}00"
+        self.currency = currency
+        options = get_web_driver_options()
+        set_ignore_certificate_error(options)
+        set_browser_as_incognito(options)
+        self.driver = get_chrome_web_driver(options)
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass

5b4ccd5fde7154e264c66e0da9936811a76d23da
tvylormvde <wernerowy@gmail.com>
2020-11-13 18:46:54 +0100

    Start implementing AmazonAPI methods

--- scraper.py
+++ scraper.py
@@ -21,13 +21,12 @@ class GenerateReport:
         pass
 
 
-
-
 class AmazonAPI:
+    
     def __init__(self, search, filters, url, currency):
         self.search = search
         self.url = url
-        self.price_filter = f"&rh=p_36%3A{filters["min"]}00-{filters["max"]}00"
+        self.price_filter = f"&rh=p_36%3A{filters['min']}00-{filters['max']}00"
         self.currency = currency
         options = get_web_driver_options()
         set_ignore_certificate_error(options)
@@ -35,9 +34,21 @@ class AmazonAPI:
         self.driver = get_chrome_web_driver(options)
 
 
+    def run(self):
+        print("Starting a script...")
+        print(f"Looking for {self.search} products...")
+        links = self.get_products_links()
+        return None
 
 
+    def get_products_links(self):
+        self.driver.get(self.url)
+        search_bar = self.driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
+        search_bar.send_keys(self.search)
+        search_bar.send_keys(Keys.RETURN)
+        time.sleep(2)
 
 
 if __name__ == "__main__":
-    pass
+    scraper = AmazonAPI("Playstation 5", FILTERS, URL, CURRENCY)
+    data = scraper.run()
